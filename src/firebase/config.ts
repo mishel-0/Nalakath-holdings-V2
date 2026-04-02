5eb4a5d23e94078903c47b685dd0e410fb702bc6
Mishel Adnan <misheladnan52@gmail.com>
2026-04-02 17:22:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5249571912-a64ac",
+  "appId": "1:133241723484:web:af52b8b0ab32af006fe499",
+  "apiKey": "AIzaSyAswWcV11nr7pNgDHFZZSfu1Pp2xqQ4ETY",
+  "authDomain": "studio-5249571912-a64ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "133241723484"
+};
